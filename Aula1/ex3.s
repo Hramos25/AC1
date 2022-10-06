1a1bbe22e7b6e741b9b442bea083a2f8371d9cf1
Hramos <rodrigomiguel471@gmail.com>
2022-10-06 18:48:15 +0100

    fix

---
+++ Aula1/ex3.s
@@ -0,0 +1,18 @@
+.data
+ .text
+ .globl main
+main: ori $v0,$0,5 
+ syscall 
+ or $t0,$0,$v0 # $t0 = $v0 = valor lido do teclado
+
+ ori $t2,$0,8 # $t2 = 8
+ add $t1,$t0,$t0 # $t1 = $t0 + $t0
+ sub $t1,$t1,$t2 # $t1 = $t1 - $t2
+
+ or $a0,$0,$t1 # $a0 = $t1 
+ ori $v0,$0,1 
+
+ syscall 
+ 
+ jr $ra 
+ 
